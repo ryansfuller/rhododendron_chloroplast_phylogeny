f7cc704604e6941b97a93a00eaf3e72bf08aaadd
Ryan S. Fuller <rsfuller@uchicago.edu>
2024-11-07 15:54:37 -0600

    Update 05.remove_mitoreads.sh

--- scripts/05.remove_mitoreads.sh
+++ scripts/05.remove_mitoreads.sh
@@ -14,6 +14,6 @@ OUTPUT=/home/rfuller/data/chloroplast_raw_seqs_July2022/ref_guided_algn_rhododen
 samtools view -f 13 -h -u -@ 3 -o ${OUTPUT}/${IND}_mito-scrubbed.bam ${INPUT}/${IND}_reverted_mitomapping.bam
 
 # -f flag of 13 corresponds to 'Read paired, Read unmapped, Mate unmapped' which will extract only \
-those reads that are properly paired and unmapped while ignoring orphaned reads whose partner was \
-mapped. In constrast, see flag 'f 5' where ALL unmapped reads are extracted regardless of whether \
-or not they have their mate and will result in singletons in the output file.                             
+# those reads that are properly paired and unmapped while ignoring orphaned reads whose partner was \
+# mapped. In constrast, see flag 'f 5' where ALL unmapped reads are extracted regardless of whether \
+# or not they have their mate and will result in singletons in the output file.                             
